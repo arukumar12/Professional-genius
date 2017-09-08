b6b7e13787d115e5e7297d927a3562b1a49a34e7
Arjun-Chandrasekaran <30828185+Arjun-Chandrasekaran@users.noreply.github.com>
2017-09-08 21:20:02 +0530

    Create alphbet.c

---
+++ alphbet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char _c;
+    printf("Enter the character");
+    scanf("%c",&_c);
+    if((_c=='a')||(_c=='b')||(_c=='c')||(_c=='d')||(_c=='e')||(_c=='f')||(_c=='g')||(_c=='h')||(_c=='i')||(_c=='j')||(_c=='k')||(_c=='l')||(_c=='m')||(_c=='n')||(_c=='o')||(_c=='p')||(_c=='q')||(_c=='r')||(_c=='s')||(_c=='t')||(_c=='u')||(_c=='v')||(_c=='w')||(_c=='x')||(_c=='y')||(_c=='z')||(_c=='A')||(_c=='B')||(_c=='C')||(_c=='D')||(_c=='E')||(_c=='F')||(_c=='G')||(_c=='H')||(_c=='I')||(_c=='J')||(_c=='K')||(_c=='L')||(_c=='M')||(_c=='N')||(_c=='O')||(_c=='P')||(_c=='Q')||(_c=='R')||(_c=='S')||(_c=='T')||(_c=='U')||(_c=='V')||(_c=='W')||(_c=='X')||(_c=='Y')||(_c=='Z'))
+    {
+        printf("The given character is an alphabet");
+    }
+    else
+    {
+            printf("The given character is not an alphabet");
+    }
+}
