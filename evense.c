02319019684a4c7985d5b2960fb68bc7db315bea
Arjun-Chandrasekaran <30828185+Arjun-Chandrasekaran@users.noreply.github.com>
2017-09-12 16:21:28 +0530

    Create evense.c

---
+++ evense.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+int s,e,i;
+printf("Enter the starting number");
+scanf("%d",&s);
+printf("Enter the ending number");
+scanf("%d",&e);
+printf("The list of odd numbers between %d and %d are:\n",s,e);
+for(i=s;i<=e;i++)
+{
+if((i%2)==0)
+{
+printf("%d\n",i);
+}
+}
+}
