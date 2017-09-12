5baacfd0465e850fa9467a27238344319bb0f167
Arjun-Chandrasekaran <30828185+Arjun-Chandrasekaran@users.noreply.github.com>
2017-09-12 15:40:49 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int n,p,r;
+printf("Enter the number");
+scanf("%d",&n);
+printf("Enter the power");
+scanf("%d",&p);
+printf("The power of the given instance is %f",pow(n,p));
+}
