6da097c790e363a77eafffb8851e03f248c18e04
ARJUN CHANDRASEKARAN <30828185+Arjun-Chandrasekaran@users.noreply.github.com>
2018-01-10 12:53:17 +0530

    Update isomorphic.c

--- isomorphic.c
+++ isomorphic.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include<string.h>
 int main(void) {
-	char a[100],b[100];
-	scanf("%s %s",a,b);
+	char in1[100],in2[100];
+	scanf("%s %s",in1,in2);
 	int m,n,i,j,u,v,w,x,y,z,flag=0;
-	m=strlen(a);
-	n=strlen(b);
+	m=strlen(in1);
+	n=strlen(in2);
 	if(m==n)
 	{
 	for(i=0;i<m;i++)
 	{
 		for(j=i+1;j<m;j++)
 		{
-			u=a[i];
-			v=a[j];
-			w=b[i];
-			x=b[j];
+			u=in1[i];
+			v=in1[j];
+			w=in2[i];
+			x=in2[j];
 			y=u-v;
 			z=w-x;
 			if(y==z)
