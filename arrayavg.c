cb7bc5a2c12440c61ea9fa66d4caea773cdfaa82
ARJUN CHANDRASEKARAN <30828185+Arjun-Chandrasekaran@users.noreply.github.com>
2018-01-18 23:35:40 +0530

    Create arrayavg.c

---
+++ arrayavg.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a[10],n,i,sum=0,avg;
+    printf("Enter the number");
+    scanf("%d",&n);
+    printf("Enter the array numbers");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum+=a[i];
+    }
+    avg=sum/n;
+    printf("The average is %d",avg);
+}
