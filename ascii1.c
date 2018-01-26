7fa72a61c5941e148981dd581d52239829d5be13
ARJUN CHANDRASEKARAN <30828185+Arjun-Chandrasekaran@users.noreply.github.com>
2018-01-26 15:53:53 +0530

    Create ascii1.c

---
+++ ascii1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+   char ch;
+   printf("Enter the character:");
+   scanf("%c",&ch);
+   printf("The ascii value of the given character %c is : %d",ch,ch);
+   return 0;
+}
