810742b08cbaa65a85ed9240427bbc9c67fcd88b
ARJUN CHANDRASEKARAN <30828185+Arjun-Chandrasekaran@users.noreply.github.com>
2018-01-26 16:06:11 +0530

    Create isogram.c

---
+++ isogram.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int i,flag=0;
+    char ch[100];
+    printf("Enter the String:");
+    scanf("%s",&ch);
+    for(i=0;ch[i]!='\0';i++)
+    {
+        if(ch[i]==ch[i+1])
+            {
+                flag=1;
+                break;
+            }
+    }
+    if(flag==1)
+    {
+        printf("The given string is not isogram");
+    }
+    else
+    {
+        printf("The given string is isogram");
+    }
+   return 0;
+}
