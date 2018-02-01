7e1382799b47d32b14a86250eb64df6af122ffd9
ARJUN CHANDRASEKARAN <30828185+Arjun-Chandrasekaran@users.noreply.github.com>
2018-02-01 08:10:42 +0000

    Create pnr.c

---
+++ pnr.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+float si,r,p,n;
+printf("\nEnter the simple interest:");
+scanf("%f",&si);
+printf("Enter the rate of interest:");
+scanf("%f",&r);
+printf("Enter the no. of years:");
+scanf("%f",&n);
+p=((si*n*r)/100);
+printf("The principal value is %.2f",p);
+}
