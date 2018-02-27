b6b60220a49b54e9d3e5366002c091cc4487da42
ARJUN CHANDRASEKARAN <30828185+Arjun-Chandrasekaran@users.noreply.github.com>
2018-02-27 16:29:11 +0530

    Update removespace.c

--- Player/Set3/removespace.c
+++ Player/Set3/removespace.c
@@ -3,18 +3,30 @@
 void main()
 {
 	char a[100],t;
-	int l,i;
+	int l,i,flag=0;
 	printf("Enter the sentence");
 	scanf("%[^\n]s",a);
 	l=strlen(a);
-	for(i=0;a[i]!='\0';i++)
+	do
 	{
-		if(a[i]==' ')
+		flag=0;
+		for(i=0;a[i]!='\0';i++)
 		{
-			t=a[i];
-			a[i]=a[i+1];
-			a[i+1]=t;
+			if(a[i]==' ')
+			{
+				t=a[i];
+				a[i]=a[i+1];
+				a[i+1]=t;
+			}
 		}
-	}
+		for(i=0;a[i]!='\0';i++)
+		{
+			if(a[i]==' ')
+			{
+				flag=1;
+				break;
+			}
+		}
+	}while(flag==1);
 	printf("\nThe string is: %s",a);
 }
