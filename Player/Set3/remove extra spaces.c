8ee462f8abc1013ea379f5e801593b3908def35e
ARJUN CHANDRASEKARAN <30828185+Arjun-Chandrasekaran@users.noreply.github.com>
2018-02-27 16:47:48 +0530

    Create remove extra spaces.c

---
+++ Player/Set3/remove extra spaces.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char a[100],t;
+	int l,i,flag=0;
+	clrscr();
+	printf("Enter the sentence");
+	scanf("%[^\n]s",a);
+	l=strlen(a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]==' ')
+		{
+			flag+=1;
+		}
+	}
+	while(flag>1)
+	{
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]==' ')
+		{
+			t=a[i];
+			a[i]=a[i+1];
+			a[i+1]=t;
+		}
+	}
+	flag--;
+	}
+	printf("\nThe string is: %s",a);
+	getch();
+}
